6a740df69859dabef77dc4755e4cba402d0619d3
Subha Chanda <subhachanda88@gmail.com>
2021-05-06 23:14:38 +0530

    Updated PORT

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Server Setup
-const port = process.env.PORT || 8000;
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log(`Server running a port ${port} in ${process.env.NODE_ENV} mode`);
 });
